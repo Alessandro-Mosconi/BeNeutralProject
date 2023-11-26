1b125ee793e29b851e1da404a33487a9af62a764
Alessandro Sassi <AleSassi@users.noreply.github.com>
2023-11-26 14:54:24 +0100

    Started implementing enemy strategies

---
+++ BeNeutral/Assets/Scripts/Enemies/EnemyPolicy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyPolicy : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject target;
+    [SerializeField]
+    public float updateInterval = 1;
+
+    private float _cumulatedTimeInterval = 0;
+    private bool _policyEnabled = true;
+    private bool _policyActiveInRunLoop = true;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        if (target == null)
+        {
+            _policyEnabled = false;
+        }
+        else
+        {
+            OnPolicyStart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_policyEnabled) {
+            return;
+        }
+
+        _cumulatedTimeInterval += Time.deltaTime;
+        if (_cumulatedTimeInterval >= updateInterval) {
+            _policyActiveInRunLoop = PolicyShouldDecide();
+            _cumulatedTimeInterval -= updateInterval;
+        }
+
+        if (_policyActiveInRunLoop)
+        {
+            ExecutePolicy();
+        }
+    }
+
+    abstract protected void OnPolicyStart();
+    abstract protected bool PolicyShouldDecide();
+    abstract protected void ExecutePolicy();
+}
