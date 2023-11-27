48e999937b0f7831f5e91d2e59a71c5316e33317
Alessandro Sassi <AleSassi@users.noreply.github.com>
2023-11-27 11:11:49 +0100

    Added support for Cinemachine cameras and moved player position update to FixedUpdate to avoid stuttering

--- BeNeutral/Assets/Scripts/Camera/FollowLastPlayer.cs
+++ BeNeutral/Assets/Scripts/Camera/FollowLastPlayer.cs
@@ -1,32 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class FollowLastPlayer : MonoBehaviour
 {
     
-    [SerializeField]
-    public Transform player1;
-    
-    [SerializeField]
-    public Transform player2;
+    [SerializeField] [NotNull] public Transform player1;
+    [SerializeField] [NotNull] public Transform player2;
+    [SerializeField] public float verticalOffset = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(Mathf.Max(player1.position.x, player2.position.x),
+            ((player1.position.y + player2.position.y) * 0.5f) + verticalOffset, transform.position.z);
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
-        if (player1.transform.position.x > player2.transform.position.x)
-        {
-            transform.position = new Vector3(player1.transform.position.x, transform.position.y, -10);
-        }
-        else
-        {
-            transform.position = new Vector3(player2.transform.position.x, transform.position.y, -10);
-        }
+        Vector3 p1Pos = player1.position;
+        Vector3 p2Pos = player2.position;
+        transform.position = new Vector3(Mathf.Max(p1Pos.x, p2Pos.x), ((p1Pos.y + p2Pos.y) * 0.5f) + verticalOffset, transform.position.z);
     }
 }
--- BeNeutral/Assets/Scripts/Player/PlayerMovement.cs
+++ BeNeutral/Assets/Scripts/Player/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         float dirX;
