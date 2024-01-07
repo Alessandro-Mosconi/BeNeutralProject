f7a5f8ab60fb04fb252f926f4ae6a3d51c6c26d3
Alessandro-Mosconi <alessandro2.mosconi@mail.polimi.it>
2024-01-07 12:02:14 +0100

    Make pressure plate pressable by objects

--- BeNeutral/Assets/Scripts/LevelExit/PressurePlate.cs
+++ BeNeutral/Assets/Scripts/LevelExit/PressurePlate.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Codice.Client.Common;
 using UnityEngine;
 
 
@@ -9,104 +10,77 @@ public class PressurePlate : MonoBehaviour
 {
     [SerializeField] private Vector3 originalPos;
 
-    private Vector3 endPosUp;
-    private Vector3 endPosDown;
-
+    private Vector3 endPos;
 
     private bool moveBack = false;
 
     private Color originalColor;
 
     [SerializeField] private Transform[] targetSpikes;
+    [SerializeField] private bool moveWhenPressed = true;
 
     private float moveDown;
     private float moveUp;
+
+    private List<GameObject> colliders = new List<GameObject>();
     
     // Start is called before the first frame update
     void Start()
     {
         originalPos = transform.position;
-        endPosUp = transform.position - new Vector3(0, 0.1f, 0);
-        endPosDown = transform.position + new Vector3(0, 0.1f, 0);
 
         originalColor = GetComponent<SpriteRenderer>().color;
     }
 
-    private void OnCollisionStay2D(Collision2D other)
-    {
-        if (other.transform.name == "Player1")
-        {
-            if (transform.position.y > endPosUp.y)
-            {
-                transform.Translate(0, -0.01f, 0 *(1.5f *Time.deltaTime));
-            }
-        }else if (other.transform.name == "Player2")
-        {
-            if (transform.position.y < endPosDown.y)
-            {
-                transform.Translate(0, 0.01f, 0 *(1.5f *Time.deltaTime));
-            }
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")
+            || other.gameObject.layer == LayerMask.NameToLayer("External-objects"))
         {
+            colliders.Add(other.gameObject);
             other.transform.parent = transform;
-            GetComponent<SpriteRenderer>().color=Color.red;
 
-            for (int i = 0; i < targetSpikes.Length; i++)
+            if (colliders.Count <= 1)
             {
-                //targetSpikes[i].Translate(0,0.6f,0);
-                targetSpikes[i].Translate(0,-0.6f,0);
+                GetComponent<SpriteRenderer>().color=Color.red;
+                if (moveWhenPressed)
+                {
+                    transform.Translate(0,-0.1f,0);
+                }
+                
+                for (int i = 0; i < targetSpikes.Length; i++)
+                {
+                    targetSpikes[i].Translate(0,-0.6f,0);
+                }
             }
+            
+            
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        moveBack = true;
-        other.transform.parent = null;
-        GetComponent<SpriteRenderer>().color=originalColor;
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")
+            || other.gameObject.layer == LayerMask.NameToLayer("External-objects"))
         {
-            for (int i = 0; i < targetSpikes.Length; i++)
-            {
-                targetSpikes[i].Translate(0, +0.6f, 0);
-            }
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (moveBack)
-        {
-            if (gameObject.name == "Pressure plateUp")
-            {
-                if (transform.position.y < originalPos.y)
-                {
-                    transform.Translate(0, 0.01f, 0);
-
-                }
-            }else if (gameObject.name == "Pressure plateDown")
-                {
-                    if (transform.position.y > originalPos.y)
-                    {
-                        transform.Translate(0, -0.01f, 0);
-
-                    }
-                }
+            other.transform.parent = null;
+            colliders.Remove(other.gameObject);
             
-        
-            else
+            if (colliders.Count == 0)
             {
-                moveBack = false;
+                GetComponent<SpriteRenderer>().color=originalColor;
+                if (moveWhenPressed)
+                {
+                    transform.Translate(0, +0.1f, 0);
+                }
+                
+                for (int i = 0; i < targetSpikes.Length; i++)
+                {
+                    targetSpikes[i].Translate(0, +0.6f, 0);
+                }
             }
-        
         }
+
     }
 
     
